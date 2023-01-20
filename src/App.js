97fe6a5eab0c39a4893caa08e0467bb47cfe0deb
Vitaliy Meshechek <meshechek.vitaliy@ukr.net>
2023-01-20 16:01:32 +0200

    start and finished

--- src/App.js
+++ src/App.js
@@ -1,39 +1,109 @@
-import React from "react";
-import React, { Component } from "react";
+import { nanoid } from 'nanoid';
+// import PropTypes from "prop-types";
 
-
+import React, { Component } from 'react';
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Filter } from 'components/Filter/Filter';
+import { ContactList } from 'components/ContactList/ContactList';
 
 // import ReactDOM from "react-dom";
 
 export class App extends Component {
   state = {
-    contacts: [],
-    name: ''
-  }
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
 
+
+  addContact = (data) => {
+    // console.log(data);
+    const contacts = this.state.contacts;
+    const coincidenceName = contacts.map((item) => item.name).includes(data.name);
+    const coincidenceNumber = contacts.map((item) => item.number).includes(data.number);
+
+    if(coincidenceName) {
+     alert(`The entered ${data.name} already exists in contacts! Please enter another name!`);
+     return;
+    } else if(coincidenceNumber) {
+      alert(`The entered ${data.number} already exists in contacts! Please enter another name!`);
+    } else if (data.name.length === 0) {
+     alert('Fields must be filled!')
+    } else {
+      const newObject = {
+        id: nanoid(), ...data,
+      }
+
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newObject],
+      }))
+      }
+    }
+
+
+   checkingContact = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+   }
+
+   deleteContact = contactItemId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactItemId),
+    }))
+   }
+
+   filterChange = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  // handleSubmit= event => {
+  //   event.preventDefault();
+  //   this.reset();
+
+  //   this.props.onSubmit({ ...this.state });
+  // }
+
+
+  // reset = () => {
+  //   this.setState(({
+  //     contacts: [{ name: '', number: '' }],
+  //     }))
+  // }
+
+//   propsFromForm = data => {
+//     console.log(data);
+//   }
+
+//   propsContactList = prop => {
+//   console.log(prop);
+//  }
+
+//   propsFilter = user => {
+//     console.log(user);
+//   }
 
   render() {
+    const {filter} = this.state;
+    const checkingContacts = this.checkingContact();
 
+    return (
+      <div>
+        <h1 title="Phonebook">Phonebook</h1>
+        <ContactForm onProps={this.addContact}/>
 
-      return (
-        <div>
-          <h1 title="Phonebook"></h1>
-          <form>
-          <label>
-          Name
-            <input
-    type="text"
-    name="name"
-    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-    required
-  />
-        </label>
+        <h2 title="Contact">Contact</h2>
 
-        <button type="submit">Sign up as {login}</button>
-
-          </form>
-        </div>
-      );
+        <Filter value={filter} onChange={this.filterChange}/>
+        {checkingContacts.length > 0 && (
+        <ContactList items={checkingContacts} onDelete={this.deleteContact}/>)}
+      </div>
+    );
   }
 }
+
